1005bc9a105a19643893e72001de3c59cf0aa7c0
Ana Carmona <ana.carmona@lifullconnect.com>
2020-12-14 00:05:27 +0100

    Day 10 Part2 WIP

--- AdventOfCode/src/main/kotlin/Advent/Day10/Day10.kt
+++ AdventOfCode/src/main/kotlin/Advent/Day10/Day10.kt
@@ -3,22 +3,85 @@ package Advent.Day10
 import java.io.File
 
 val allLines = arrayListOf<String>()
-var joltArrangements = mutableListOf<Long>()
-
+var joltArrangements = mutableListOf<MutableList<Long>>()
+var maxVal:Long =0
+var joltAdapterAvailable = mutableListOf<Long>()
 
 fun main() {
-    readInputFile("src/main/resources/Advent/Day10/Day10Input.txt")
+
+    readInputFile("src/main/resources/Advent/Day10/Day10Tiny.txt")
     println(firstStep()) //2048
+    println(secondStep())
+    println(joltArrangements)
 }
 
+fun secondStep():Int{
+    joltAdapterAvailable = setJoltInitialList()
+    for (index in 0 .. joltAdapterAvailable.lastIndex)
+    {
+        joltArrangements.add(searchSolution(index, joltAdapterAvailable, true))
+    }
+    //joltArrangements =  joltArrangements.distinct().toMutableList()
+    return joltArrangements.count()
+}
+
+fun searchSolution(index: Int, joltAdapterAvailable: MutableList<Long>, firstAttempt: Boolean): MutableList<Long> {
+    var innerJoltList = joltAdapterAvailable.toMutableList()
+    if(index>joltAdapterAvailable.lastIndex) return joltAdapterAvailable
+    val window = setJoltWindow(index,innerJoltList)
+    if(canBeSolution1(window,1, firstAttempt))
+        return (searchSolution(index+1,innerJoltList,false))
+    if(canBeSolution2(window,2, firstAttempt)) {
+        if(index+1 < innerJoltList.lastIndex) innerJoltList.removeAt(index+1)
+        return (searchSolution(index + 2, innerJoltList, false))
+    }
+    if(canBeSolution3(window,3,firstAttempt)) {
+        if(index+1 < innerJoltList.lastIndex) innerJoltList.removeAt(index+1)
+        if(index+2 < innerJoltList.lastIndex) innerJoltList.removeAt(index+2)
+        return (searchSolution(index + 3, innerJoltList, false))
+    }
+    return joltAdapterAvailable
+}
+
+fun canBeSolution1(window: MutableList<Long>, plus: Int, firstAttempt: Boolean): Boolean {
+    if (!firstAttempt && !canEvaluate(window, 0)) return false
+    val reference = window[0] + plus
+    if (canEvaluate(window,1) && reference == window[1]) return true
+    return false
+}
+
+fun canBeSolution2(window: MutableList<Long>, plus: Int, firstAttempt: Boolean): Boolean {
+    if (!firstAttempt && !canEvaluate(window, 0)) return false
+    val reference = window[0] + plus
+    if (canEvaluate(window,2) && (reference == window[1] || reference == window[2])) return true
+    return false
+}
+
+fun canBeSolution3(window: MutableList<Long>, plus: Int, firstAttempt: Boolean): Boolean {
+    if (!firstAttempt && !canEvaluate(window, 0)) return false
+    val reference = window[0] + plus
+    if (canEvaluate(window,3) && (reference == window[1] || reference == window[2] || reference == window[3])) return true
+    return false
+}
+
+private fun canEvaluate(window: MutableList<Long>, index: Int): Boolean {
+    val comp: Long = 0
+    if (window[index] == comp) return false
+    return true
+}
+
+fun setJoltWindow(initIndex: Int,joltAdapterAvailable: MutableList<Long>): MutableList<Long>{
+    var finalIndex = initIndex + 3
+    if(finalIndex > joltAdapterAvailable.lastIndex)
+        finalIndex = joltAdapterAvailable.lastIndex
+    val window = joltAdapterAvailable.subList(initIndex,finalIndex+1).toMutableList()
+    if(window.size<4) for(c in window.size-1 .. 2) window.add(0)
+    return window
+}
+
+//region firstCase
 fun firstStep():Long{
-    //Init
-    var joltAdapterAvailable = mutableListOf<Long>()
-    joltAdapterAvailable.add(0)
-    joltAdapterAvailable.addAll(fillJoltBagOrdered())
-    val max:Long= joltAdapterAvailable.maxOrNull()?.plus(3)!!
-    joltAdapterAvailable.add(max)
-    //Body
+    var joltAdapterAvailable = setJoltInitialList()
     var finished = false
     var multiple1:Long =0
     var multiple3:Long =0
@@ -43,16 +106,10 @@ private fun evaluateCase(window: List<Long>): Pair<Long, Long> {
 
 }
 
-
 fun noMoreWindowsAvailable(joltAdapterAvailable: MutableList<Long>): Boolean {
     if(joltAdapterAvailable.size<=1) return true
     return false
 }
-
-fun fillJoltBagOrdered(): MutableList<Long>{
-    return allLines.map(String::toLong).sorted() as MutableList<Long>
-}
-
 
 fun setJoltWindow(joltAdapterAvailable: MutableList<Long>): MutableList<Long>{
     val window = joltAdapterAvailable.take(4) as MutableList<Long>
@@ -60,6 +117,21 @@ fun setJoltWindow(joltAdapterAvailable: MutableList<Long>): MutableList<Long>{
     joltAdapterAvailable.removeAt(0)
     return window
 }
+//endregion firstCase
+
+//region common
+fun fillJoltBagOrdered(): MutableList<Long>{
+    return allLines.map(String::toLong).sorted() as MutableList<Long>
+}
+
+private fun setJoltInitialList(): MutableList<Long> {
+    var joltAdapterAvailable = mutableListOf<Long>()
+    joltAdapterAvailable.add(0)
+    joltAdapterAvailable.addAll(fillJoltBagOrdered())
+    maxVal= joltAdapterAvailable.maxOrNull()?.plus(3)!!
+    joltAdapterAvailable.add(maxVal)
+    return joltAdapterAvailable
+}
 
 fun readInputFile(fileName: String) = File(fileName).forEachLine { allLines.add(it) }
-
+//endregion common
